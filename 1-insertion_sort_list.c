8a1f302513085a346f242c4259e125089354edad
Thorlak2202 <darkdrakkar@gmail.com>
2020-02-03 15:08:23 -0500

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,6 +11,9 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current, *tmp, *swapper;
 	int flag;
 
+	if (list == NULL || *list == NULL)
+		return;
+
 	current = (*list)->next;
 	while (current != NULL)
 	{
